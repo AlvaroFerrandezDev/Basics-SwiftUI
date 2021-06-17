fbd096fd48aabbbd65cfd8d71b4ce217cff26844
Álvaro Ferrández <alvaro.ferrandez.dev@gmail.com>
2021-06-17 11:24:20 +0200

    Initial Commit

---
+++ Basics-SwiftUI/Basics_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basics_SwiftUIApp.swift
+//  Basics-SwiftUI
+//
+//  Created by Álvaro Ferrández Gómez on 17/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Basics_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
